6e67063a5ce8c083c76516930de0fcd3eedd54f0
John1211 <shtangzenglei@163.com>
2018-12-04 11:52:14 +0800

    Gradle plugin

--- buildsrc/src/main/groovy/com/tzl/R2Log.groovy
+++ buildsrc/src/main/groovy/com/tzl/R2Log.groovy
@@ -1,12 +1,14 @@
 package com.tzl
 
-class R2Log{
+class R2Log {
 
     static final String TAG = "R2Tool"
 
+//    private static logger =  Logger.getLogger("")
 
     static void log(String msg) {
-        println(TAG+msg)
+        println(TAG + msg)
     }
+
 
 }
